0e1bcbcebc43611c13b703421c71d8ae21068946
Sauceage2 <sauceage@gmail.com>
2025-01-30 20:23:21 -0500

    addeed party schema and create party in server

--- server/index.js
+++ server/index.js
@@ -10,3 +10,34 @@ var io = require("socket.io")(server);
 
 app.use(express.json())
 
+io.on('connection', (socket) => {
+    console.log('connected');
+
+    socket.on('create-game', async ({ username, name, partySize, maxRounds }) => {
+        try {
+            const existingRoom = await Party.findOne({ name });
+            if (existingRoom) {
+                socket.emit('notCorrectGame', "Room with that name already exists!");
+                return;
+            }
+            let party = new Party();
+            party.name = name;
+            party.partySize = partySize;
+            party.maxRounds = maxRounds;
+
+            let player = {
+                socketID: socket.id,
+                username,
+                isPartyLeader: true,
+            }
+
+            party.players.push(player);
+            party = await party.save();
+            socket.join(name);
+            io.to(name).emit('updateParty', party);
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+});
